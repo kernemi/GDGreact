f7eaceda245e9d6dbfa1718014cd384f2b8132fb
kernemi <149039271+kernemi@users.noreply.github.com>
2024-12-25 15:09:08 +0300

    Create App.jsx

---
+++ p1/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import GroupMembers from './Groupmembers';
+
+const App = () => {
+  return (
+    <div>
+      <GroupMembers />
+    </div>
+  );
+};
+
+export default App;
